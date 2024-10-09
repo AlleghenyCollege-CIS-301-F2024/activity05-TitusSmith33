acae64da1de3779cdd58b7bd9332628cf53f3900
TitusSmith33 <smith03@allegheny.edu>
2024-10-09 09:43:33 -0400

    update: added MANOVA testing

--- src/manova.r
+++ src/manova.r
@@ -1,6 +1,6 @@
 # Manova demo using Skulls dataset.
 
-# Add Your Name Here
+# Titus Smith
 
 rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
 
@@ -63,4 +63,20 @@ ggplot(means_long, aes(x = epoch, y = mean_value, fill = measurement)) +
 # Add manova code below
 ##################################
 
-# TODO
+skulls.manova1 <- manova(cbind(mb, bh, bl, nh) ~ as.factor(epoch), data = skulls)
+# prepare different types of MANOVA test
+
+summary(skulls.manova1, test = "Hotelling-Lawley")
+summary(skulls.manova1, test = "Roy")
+summary(skulls.manova1, test = "Pillai")
+summary(skulls.manova1, test = "Wilks")
+
+summary.aov(skulls.manova1)
+cat("summary of aov")
+
+skulls.manova2 <-
+  manova(cbind(mb, bh, bl, nh) ~ as.factor(epoch),
+         data = skulls,
+         subset = as.factor(epoch) %in% c("c4000BC", "c200BC"))
+View(skulls.manova2)
+summary(skulls.manova2)
